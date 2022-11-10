77d9151c46e5b55e7213d1c5da4c0bab6d1cb6f8
Derek Harter <derek@harter.pro>
2022-11-10 09:19:37 -0600

    Add examples of x86 and arm assembly
    More examples of assembly using 64 bit gnu assembler
    gas.  Examples for both x86-64 and arm64 (aarch64)
    architectures.

--- ch13-x86-assembly-examples/security-2.s
+++ ch13-x86-assembly-examples/security-2.s
@@ -1,17 +1,18 @@
 ##########################################################################
 #
-# Program: hello_world_gas_solution_3
+# Program: hello_world_gas_solution_1
 #
 # Date: 11/08/2022
 #
 # Author: Travis Phillips
 # Source: https://www.secureideas.com/blog/2021/06/linux-x86-assembly-how-to-make-our-hello-world-usable-as-an-exploit-payload.html
 #
-# Purpose: An updated hello world program safe for shellcode that is
-#          reduced in size compared to previous versions.
+# Purpose: An updated hello world program in x86 assembly for GAS that
+#          fixes the program so that the hello world string is no longer
+#          at a fixed address
 #
-# Compile: as --march=i386 --32 ./hello_world_gas_solution_3.s -o hello_world_gas_solution_3.o
-#    Link: ld -m elf_i386 hello_world_gas_solution_3.o -o hello_world_gas_solution_3
+# Compile: as --march=i386 --32 ./hello_world_gas_solution_1.s -o hello_world_gas_solution_1.o
+#    Link: ld -m elf_i386 hello_world_gas_solution_1.o -o hello_world_gas_solution_1
 #
 ##########################################################################
 .global  _start                 # we must export the entry point to the
@@ -26,45 +27,39 @@ _start:
     jmp my_string               # Jump to the my_string label.
 payload:
     ######################################
-    # syscall - write(1, msg, 14);
+    # syscall - write(1, msg, len);
     ######################################
-    push   $4
-    pop    %eax                 # 4 = Syscall number for Write()
+    mov    $4,%eax              # 4 = Syscall number for Write()
 
-    push   $1                   # Push 1 to the stack.
-    pop    %ebx                 # File Descriptor to write to
+    mov    $1,%ebx              # File Descriptor to write to
                                 # In this case: STDOUT is 1
 
     pop    %ecx                 # Pop the string address pointer
                                 # off the stack into ecx.
 
-    push   $0x0e                # Push the length directly.
-    pop    %edx                 # pop it into edx.
+    mov    $len,%edx            # The length of string to print
+                                # which is 14 characters
 
     int    $0x80                # Poke the kernel and tell it to run the
                                 # write() call we set up
 
     ######################################
-    # syscall - exit(bytes_printed);
+    # syscall - exit(0);
     ######################################
-    # exchange eax and ebx registers. This
-    # instruction is only one byte!
-    #
-    # ebx currently has 1 for STDOUT. This
-    # is also the syscall for exit().
-    #
-    # eax has the number of bytes written!
-    # This will invoke exit with a status
-    # code of the number of bytes written!
-    ######################################
-    xchg    %ebx,%eax           # Swap eax and ebx
-    int     $0x80               # Poke kernel. This will end the program.
+    mov    $1,%al               # 1 = Syscall for Exit()
+    mov    $0,%ebx              # The status code we want to provide.
+    int    $0x80                # Poke kernel. This will end the program.
 
 my_string:
-    call   payload              # Call to the payload label. This will
+    call   payload              # Call the payload label. This will
                                 # push the pointer to msg onto the stack
                                 # as a return address.
 
 msg:
-    .ascii    "Hello, World!\n" # Declare a label "msg" which has
+    .ascii "Hello, World!\n"    # Declare a label "msg" which has
                                 # our string we want to print.
+
+    len = . - msg               # "len" will calculate the current
+                                # offset minus the "msg" offset.
+                                # this should give us the size of
+                                # "msg".
